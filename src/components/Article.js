9a73dde172aba6d76a3f3ef995d3bc3e44ce2f4f
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-11-13 16:02:39 +0300

    react tests

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>Hello from the Article!</div>;
 }
 
 export default Article;
